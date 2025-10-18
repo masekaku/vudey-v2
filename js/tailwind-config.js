423d64a770b1fbb8bbd716531e680c1753997cf3
masekaku <aguslistiyanto27@gmail.com>
2025-10-19 00:31:24 +0700

    Create tailwind-config.js

---
+++ js/tailwind-config.js
@@ -0,0 +1,20 @@
+window.tailwind = {
+  config: {
+    theme: {
+      extend: {
+        colors: {
+          base: '#FAFAF3',
+          dark: '#292C30',
+          grayish: '#979AA0',
+          redish: '#DE4C4C',
+          yellowish: '#E1C941',
+          pinkish: '#EC848C'
+        },
+        borderRadius: {
+          xl: '1rem',
+          '2xl': '1.5rem'
+        }
+      }
+    }
+  }
+};
