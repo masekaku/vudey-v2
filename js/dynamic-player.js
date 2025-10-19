19d2a1a3b4d25ba24b2215be4252d977fc52ffd3
masekaku <aguslistiyanto27@gmail.com>
2025-10-20 05:08:28 +0700

    Update dynamic-player.js

--- js/dynamic-player.js
+++ js/dynamic-player.js
@@ -19,34 +19,6 @@ export function renderMain(root) {
           <div class="text-xl mb-1">❤</div>
           Iklan mungkin menyebalkan, tetapi itu satu-satunya cara kami menjaga server tetap hidup. Terima kasih atas kesabaran Anda 💛
         </div>
-
-        <!-- Sponsor Buttons -->
-<div class="mt-6 flex flex-wrap gap-3 justify-center">
-  <button 
-    onclick="window.open('https://workplacecakefaculty.com/wnxneai5xf?key=f234c1281833908a12385bbbc9881cf7', '_blank')" 
-    class="sponsor-btn bg-yellow-200 border-4 border-[color:var(--ink2)] rounded-xl px-4 py-2 font-bold hover:translate-y-[-2px] hover:translate-x-[-2px] hover:shadow-[4px_4px_0_var(--ink2)] transition">JAPAN</button>
-
-  <button 
-    onclick="window.open('https://workplacecakefaculty.com/uczmxmzewu?key=ffd852fa421f91c5b97b8e043e98844f', '_blank')" 
-    class="sponsor-btn bg-yellow-200 border-4 border-[color:var(--ink2)] rounded-xl px-4 py-2 font-bold hover:translate-y-[-2px] hover:translate-x-[-2px] hover:shadow-[4px_4px_0_var(--ink2)] transition">INDO</button>
-
-  <button 
-    onclick="window.open('https://workplacecakefaculty.com/ibhnbx77?key=245d5f18a70ac6f3ccf7041441886168', '_blank')" 
-    class="sponsor-btn bg-yellow-200 border-4 border-[color:var(--ink2)] rounded-xl px-4 py-2 font-bold hover:translate-y-[-2px] hover:translate-x-[-2px] hover:shadow-[4px_4px_0_var(--ink2)] transition">PELAJAR</button>
-</div>
-
-        <!-- Iklan -->
-<div class="mt-6 p-6 border-4 border-dashed border-[color:var(--grayish)] rounded-xl text-[color:var(--grayish)] text-lg text-center">
-  <!-- Script Iklan -->
-</div>
-
-        <!-- Footer -->
-<footer class="mt-6 py-4 text-xs text-[color:var(--grayish)] text-center">
-  © 2025 <span class="font-semibold">VidOst</span> — 
-  <a href="https://vid.ost.web.id/protocol" class="hover:underline hover:text-[color:var(--ink2)] transition">Protocol</a> | 
-  <a href="https://vid.ost.web.id/terms" class="hover:underline hover:text-[color:var(--ink2)] transition">Terms</a> | 
-  <a href="https://vid.ost.web.id/contact" class="hover:underline hover:text-[color:var(--ink2)] transition">Contact</a>
-</footer>
       </div>
     </div>
   `;
