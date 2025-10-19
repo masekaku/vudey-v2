04378ac1d582354ec4ce86fef352e5b9ce3700e5
masekaku <aguslistiyanto27@gmail.com>
2025-10-20 06:30:28 +0700

    Update lol.js

--- vidost/lol.js
+++ vidost/lol.js
@@ -10,6 +10,13 @@ export function initVidostUI(root = document.body) {
     </div>
     <div class="mt-6 p-6 border-4 border-dashed border-[color:var(--grayish)] rounded-xl text-[color:var(--grayish)] text-lg text-center">
       <!-- Script Iklan -->
+    <div id="frame" style="width: 300px;margin: auto;z-index: 99998;height: auto">
+          <iframe data-aa='2414290' src='//ad.a-ads.com/2414290/?size=300x250&background_color=transparent&title_color=292C30&title_hover_color=979AA0&text_color=DE4C4C&link_color=1C1E22&link_hover_color=2E3238'
+                            style='border:0; padding:0; width:300px; height:250px; overflow:hidden;display: block;margin: auto'></iframe>
+          <div style="width: 300px;margin:auto;position: absolute;left: 0;right: 0">
+            <a target="_blank" style="display:inline-block;font-size: 13px;color: #263238;padding: 4px 10px;background: #F8F8F9;text-decoration: none; border-radius: 0 0 4px 4px;" id="frame-link" href="https://aads.com/campaigns/new/?source_id=2414290&source_type=ad_unit&partner=2414290">Advertise here</a>
+          </div>
+        </div>
     </div>
     <footer class="mt-6 py-4 text-xs text-[color:var(--grayish)] text-center">
       © 2025 <span class="font-semibold">VidOst</span> —
