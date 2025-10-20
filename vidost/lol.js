5c843711a6c10480f6a9fb5e8e6c3a5b71d9e2df
masekaku <aguslistiyanto27@gmail.com>
2025-10-20 07:41:31 +0700

    Update lol.js

--- vidost/lol.js
+++ vidost/lol.js
@@ -23,7 +23,27 @@ export function initVidostUI(root = document.body) {
       <a href="/protocol" class="hover:underline hover:text-[color:var(--ink2)] transition">Protocol</a> |
       <a href="/terms" class="hover:underline hover:text-[color:var(--ink2)] transition">Terms</a> |
       <a href="/contact" class="hover:underline hover:text-[color:var(--ink2)] transition">Contact</a>
-    </footer>
+    <!-- Histats Counter -->
+  <div id="histats_counter" class="mt-3 flex justify-center"></div>
+  <script type="text/javascript">
+    var _Hasync = _Hasync || [];
+    _Hasync.push(['Histats.start', '1,4985432,4,5,172,25,00011111']);
+    _Hasync.push(['Histats.fasi', '1']);
+    _Hasync.push(['Histats.track_hits', '']);
+    (function() {
+      var hs = document.createElement('script');
+      hs.type = 'text/javascript';
+      hs.async = true;
+      hs.src = ('//s10.histats.com/js15_as.js');
+      (document.getElementsByTagName('head')[0] || document.getElementsByTagName('body')[0]).appendChild(hs);
+    })();
+  </script>
+  <noscript>
+    <a href="/" target="_blank">
+      <img src="//sstatic1.histats.com/0.gif?4985432&101" alt="Histats" border="0" class="inline-block opacity-70 hover:opacity-100 transition" />
+    </a>
+  </noscript>
+</footer>
   `;
 
   const target = root.querySelector(".app-centered") || root;
